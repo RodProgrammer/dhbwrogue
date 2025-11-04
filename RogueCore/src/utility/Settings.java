4dc6bb496ad6a8cce6c40453b680e2803658dee6
RodProgrammer <robinboesch3@gmx.de>
2025-11-04 10:55:09 -0800

    updated gitignore and changed FOV

--- RogueCore/src/utility/Settings.java
+++ RogueCore/src/utility/Settings.java
@@ -8,7 +8,7 @@ public class Settings {
     public static final int SCREEN_HEIGHT = 720;
 
     public static final int TILE_SIZE = 16;
-    public static final int SCALED_TILE_SIZE = TILE_SIZE * 2;
+    public static final int SCALED_TILE_SIZE = TILE_SIZE * 3;
 
     public static final int TPS = 60;
 
--- RogueCore/src/utility/Utility.java
+++ RogueCore/src/utility/Utility.java
@@ -1,7 +1,25 @@
 package utility;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class Utility {
 
     private Utility(){}
+
+    /**
+     * This exists so the renderer doesn't have to rescale each time it draws the picture,
+     * but we will resize the Picture beforehand and then we gucci
+     */
+    public static BufferedImage scaleImage(BufferedImage originalImage) {
+
+        BufferedImage scaledImage = new BufferedImage(Settings.SCALED_TILE_SIZE, Settings.SCALED_TILE_SIZE, 2);
+        Graphics2D graphics2D = scaledImage.createGraphics();
+        graphics2D.drawImage(originalImage, 0, 0, Settings.SCALED_TILE_SIZE, Settings.SCALED_TILE_SIZE, null);
+        graphics2D.dispose();
+
+        return scaledImage;
+    }
+
 
 }
