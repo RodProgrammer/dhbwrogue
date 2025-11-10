eb195f4b2bccf679a7e140b7fcb88284caa97860
RodProgrammer <robinboesch3@gmx.de>
2025-11-10 16:18:53 +0100

    just added a basic image to all Tiles

--- RogueCore/src/tiles/Tile.java
+++ RogueCore/src/tiles/Tile.java
@@ -12,11 +12,16 @@ public class Tile {
     private transient BufferedImage image;
 
     public Tile(int x, int y) {
-        rect = new Rectangle(x,y, Settings.SCALED_TILE_SIZE,Settings.SCALED_TILE_SIZE);
+        rect = new Rectangle(x,y, Settings.SCALED_TILE_SIZE, Settings.SCALED_TILE_SIZE);
         rect.x = x;
         rect.y = y;
         rect.width = Settings.SCALED_TILE_SIZE;
         rect.height = Settings.SCALED_TILE_SIZE;
+
+        image = new BufferedImage(rect.width, rect.height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = image.createGraphics();
+        g2d.setColor(Color.MAGENTA);
+        g2d.drawRect(x, y, rect.width, rect.height);
     }
 
     public void render(Graphics2D g) {
@@ -24,10 +29,10 @@ public class Tile {
     }
 
     public void tick() {
-
+        //TODO: possible animation?
     }
 
-    public Rectangle getRect() {
+    public Rectangle getRectangle() {
         return rect;
     }
 
